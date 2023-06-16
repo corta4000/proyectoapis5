3db570c00af7693280d9bcaf207ab7059c710441
corta4000 <116226246+corta4000@users.noreply.github.com>
2023-06-15 23:59:28 -0300

    Create registro.js

---
+++ front/registro.js
@@ -0,0 +1,67 @@
+// Obtener referencia al formulario de registro
+const registrationForm = document.getElementById('registration-form');
+
+// Agregar un event listener para el envío del formulario
+registrationForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevenir el envío del formulario
+
+  // Abrir una nueva pestaña con la página de registro
+  window.open('login.html', '_blank');
+});
+
+class Registrate {
+    constructor(){
+        /*
+        Se instancia el formualario del lado del JS para que accione al momento
+        del submit (evento del formulario)
+        */
+        const formRegistro = document.querySelector('#registration-form');
+        this._registrar = this._registrar.bind(this);
+        formRegistro.addEventListener('submit', this._registrar);  
+        console.log('form: ' + formRegistro);  
+    }
+
+    _registrar(event) {
+        event.preventDefault();
+
+        console.log('values')
+
+        //Instancio los input para luego obtener sus valores
+        const inputName = document.querySelector('#name');
+        console.log('name' + inputName.value)
+        const inputEmail = document.querySelector('#mail');
+        console.log('mail' + inputEmail.value)
+        const inputPassword = document.querySelector('#password');
+        console.log('password' + inputPassword.value)
+
+
+        this.singup({
+            nombre : inputName.value,
+            email : inputEmail.value,
+            password : inputPassword.value
+        })
+        .then(result => {
+            //Promesa hacer algo una vez obtenido el resuldao del servidor
+            
+        });
+                
+    }
+
+    //Metodo para hacer el fetch al server.
+    singup(postBody) {
+        const fetchOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postBody)
+        };
+        
+        return fetch('registro', fetchOptions);
+
+    }
+
+}
+
+const registrare = new Registrate();
